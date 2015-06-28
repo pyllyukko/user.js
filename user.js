ab0951d5e00f844912b36bb38bb80c642f7f76df
pyllyukko <pyllyukko@maimed.org>
2015-06-28 19:23:36 +0300

    media.gmp-manager.url

--- user.js
+++ user.js
@@ -256,6 +256,7 @@ user_pref("browser.casting.enabled",				false);
 // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_media-capabilities
 // http://andreasgal.com/2014/10/14/openh264-now-in-firefox/
 user_pref("media.gmp-gmpopenh264.enabled",			false);
+user_pref("media.gmp-manager.url",				"");
 
 // https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_speculative-pre-connections
 user_pref("network.http.speculative-parallel-limit",		0);
