fd6cf46447dcd4b8e44246c525d715b7ea9d8126
pyllyukko <pyllyukko@maimed.org>
2015-06-28 20:34:45 +0300

    Enable safe browsing (web forgeries)
    This was previously disabled, as it seemed that it could leak
    information by looking up visited sites from Google (see
    http://kb.mozillazine.org/Browser.safebrowsing.remoteLookups).
    
    The lookup method was removed from Firefox a while ago (see
    https://bugzil.la/388652) and also from this user.js in commit
    3518ff05caa65c972e40a0e61ee7dc246482672b.
    
    Safe browsing should not leak information about your browsing, for
    more details, see:
    https://github.com/pyllyukko/user.js/issues/22#issuecomment-116293317
    
    Closes #22.

--- user.js
+++ user.js
@@ -214,9 +214,9 @@ user_pref("browser.selfsupport.url",				"");
 // CIS 2.3.4 Block Reported Web Forgeries
 // http://kb.mozillazine.org/Browser.safebrowsing.enabled
 // http://kb.mozillazine.org/Safe_browsing
-//
-// "or submit URLs to a third party"
-user_pref("browser.safebrowsing.enabled",		false);
+// https://support.mozilla.org/en-US/kb/how-does-phishing-and-malware-protection-work
+// http://forums.mozillazine.org/viewtopic.php?f=39&t=2711237&p=12896849#p12896849
+user_pref("browser.safebrowsing.enabled",		true);
 
 // CIS 2.3.5 Block Reported Attack Sites
 // http://kb.mozillazine.org/Browser.safebrowsing.malware.enabled
