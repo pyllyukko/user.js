fab377e69cccab663722e14fcd82f19d46254b8d
CHEF-KOCH <CHEF-KOCH@users.noreply.github.com>
2015-10-13 03:38:33 +0200

    added JavaScript exploit protection (optional)
    Optional protection against eval() to execute data like it was code.

--- user.js
+++ user.js
@@ -8,6 +8,13 @@
  *                                                                            *
  ******************************************************************************/
 
+// Protection against Javascript exploits to block eval() 
+// See: https://hackademix.net/2011/09/29/script-surrogates-quick-reference/
+//noscript.surrogate.noeval.replacement,		window.eval = null;document.eval=null;
+//noscript.surrogate.noeval.sources,		@^http://[a-z]+[^/]+\.[a-z]+(?:/|$);
+//noscript.surrogate.noeval.exceptions;
+
+
 // disable Location-Aware Browsing
 // http://www.mozilla.org/en-US/firefox/geolocation/
 user_pref("geo.enabled",		false);
