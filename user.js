pyllyukko/user.js

commit e05520e736e23d3d3cd9f87c9bf14a641a2d0261

Disabled face detection
^^

user.js
@@ -85,6 +85,9 @@ user_pref("webgl.disabled",		true);
  *                                                                            *
  ******************************************************************************/
 
+// Disable face detection by default
+user_pref("camera.control.face_detection.enabled";    false);
+
 // Default search engine
 //user_pref("browser.search.defaultenginename",		"DuckDuckGo");
 

commit c6b1c0c6d3a0d8111f1264a97cc56b93ba289903

Fixed a syntax bug introduced in e05520e

user.js
@@ -86,7 +86,7 @@ user_pref("webgl.disabled",		true);
  ******************************************************************************/
 
 // Disable face detection by default
-user_pref("camera.control.face_detection.enabled";    false);
+user_pref("camera.control.face_detection.enabled",    false);
 
 // Default search engine
 //user_pref("browser.search.defaultenginename",		"DuckDuckGo");