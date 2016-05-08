942cda09b55f6a67a58032cff606eaa01254f49a
pyllyukko <pyllyukko@maimed.org>
2016-05-08 10:17:36 +0300

    extensions.pocket.enabled -> false
    Fixes #143

--- user.js
+++ user.js
@@ -301,6 +301,8 @@ user_pref("browser.safebrowsing.downloads.remote.enabled",		false);
 // Disable pocket
 // https://support.mozilla.org/en-US/kb/save-web-pages-later-pocket-firefox
 user_pref("browser.pocket.enabled",		false);
+// https://github.com/pyllyukko/user.js/issues/143
+user_pref("extensions.pocket.enabled",		false);
 
 /******************************************************************************
  * automatic connections                                                      *
