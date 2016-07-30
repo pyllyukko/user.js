71816d82097699d93c634a2785c12d48b6f8c5ba
Francois Marier <francois@mozilla.com>
2016-07-29 17:52:47 -0700

    Remove history preferences which have been removed from Firefox
    https://dxr.mozilla.org/mozilla-central/rev/2ea3d51ba1bb9f5c3b6921c43ea63f70b4fdf5d2/toolkit/components/places/Database.cpp#1690-1696
    https://dxr.mozilla.org/mozilla-central/search?q=browser.history_expire_days
    https://dxr.mozilla.org/mozilla-central/search?q=browser.history_expire_visits
    https://bugzilla.mozilla.org/show_bug.cgi?id=568613

--- user.js
+++ user.js
@@ -479,16 +479,6 @@ user_pref("browser.cache.disk_cache_ssl",			false);
 // CIS Version 1.2.0 October 21st, 2011 2.5.2 Disallow Credential Storage
 user_pref("signon.rememberSignons",				false);
 
-// CIS Version 1.2.0 October 21st, 2011 2.5.4 Delete History and Form Data
-// http://kb.mozillazine.org/Browser.history_expire_days
-user_pref("browser.history_expire_days",			0);
-
-// http://kb.mozillazine.org/Browser.history_expire_sites
-user_pref("browser.history_expire_sites",			0);
-
-// http://kb.mozillazine.org/Browser.history_expire_visits
-user_pref("browser.history_expire_visits",			0);
-
 // CIS Version 1.2.0 October 21st, 2011 2.5.5 Delete Download History
 // Zero (0) is an indication that no download history is retained for the current profile.
 user_pref("browser.download.manager.retention",			0);
