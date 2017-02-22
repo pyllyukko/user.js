a97df976b2615c07b4e2419881f2dfa2b7205b2f
pyllyukko <pyllyukko@maimed.org>
2017-02-22 19:51:40 +0200

    Remove few leading spaces

--- user.js
+++ user.js
@@ -10,8 +10,8 @@
 // https://wiki.mozilla.org/Platform/GFX/HardwareAcceleration
 // https://www.macromedia.com/support/documentation/en/flashplayer/help/help01.html
 // https://github.com/dillbyrne/random-agent-spoofer/issues/74
- user_pref("gfx.direct2d.disabled",				true);
- user_pref("layers.acceleration.disabled",			true);
+user_pref("gfx.direct2d.disabled",				true);
+user_pref("layers.acceleration.disabled",			true);
 
 
 /******************************************************************************
