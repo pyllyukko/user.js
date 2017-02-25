6b1f6554f6fba63427f5f7758f0adbf6178519c3
nodiscc <nodiscc@gmail.com>
2017-02-25 14:13:36 +0100

    Disable speech synthesis
    Add additional documentation links
    Fixes item 2021 of #208

--- user.js
+++ user.js
@@ -79,8 +79,13 @@ user_pref("dom.enable_performance",				false);
 
 // Speech recognition
 // https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
 // https://wiki.mozilla.org/HTML5_Speech_API
 user_pref("media.webspeech.recognition.enable",			false);
+
+// Speech synthesis
+// https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+user_pref("media.webspeech.synth.enabled", false);
 
 // Disable getUserMedia screen sharing
 // https://mozilla.github.io/webrtc-landing/gum_test.html
