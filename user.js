04063e45e0bcb6d4ff142325b62fa46d98c4ef77
pyllyukko <pyllyukko@maimed.org>
2017-03-02 20:28:26 +0200

    Removed deprecated network.seer.enabled setting
    network.predictor.enabled does the job
    
    Fixes #228

--- user.js
+++ user.js
@@ -423,8 +423,6 @@ user_pref("network.dns.blockDotOnion",				true);
 
 // https://wiki.mozilla.org/Privacy/Reviews/Necko
 user_pref("network.predictor.enabled",				false);
-// https://wiki.mozilla.org/Privacy/Reviews/Necko#Principle:_Real_Choice
-user_pref("network.seer.enabled",				false);
 
 // http://kb.mozillazine.org/Browser.search.suggest.enabled
 user_pref("browser.search.suggest.enabled",			false);
