c4db3080eda7d6b7f10153520fe59871fa30a69a
nodiscc <nodiscc@gmail.com>
2017-03-26 18:31:17 +0200

    Disable sending Firefox crash reports to Mozilla servers, fixes item 0305 of #208

--- user.js
+++ user.js
@@ -368,6 +368,14 @@ user_pref("experiments.manifest.uri",				"");
 // PREF: Disallow Necko to do A/B testing
 user_pref("network.allow-experiments",			false);
 
+// PREF: Disable sending Firefox crash reports to Mozilla servers
+// https://wiki.mozilla.org/Breakpad
+// http://kb.mozillazine.org/Breakpad
+// https://dxr.mozilla.org/mozilla-central/source/toolkit/crashreporter
+// https://bugzilla.mozilla.org/show_bug.cgi?id=411490
+// A list of submitted crash reports can be found at about:crashes
+user_pref("breakpad.reportURL",			"");
+
 // PREF: Disable the UITour backend
 // https://trac.torproject.org/projects/tor/ticket/19047#comment:3
 user_pref("browser.uitour.enabled",				false);
