d70077af95bedd173517f444ede59fa68234739d
nodiscc <nodiscc@gmail.com>
2017-03-26 18:20:46 +0200

    Disallow Necko to do A/B testing, fixes itme 0341 of #208, see https://dxr.mozilla.org/mozilla-central/source/modules/libpref/init/all.js#1350 set experiments manifest uri to blank, even though experiments are already disabled; update previously broken links in experiments/telemetry section fixes item 0340 of #208

--- user.js
+++ user.js
@@ -352,16 +352,21 @@ user_pref("devtools.debugger.force-local",			true);
 
 // PREF: Disable Mozilla telemetry/experiments
 // https://wiki.mozilla.org/Platform/Features/Telemetry
-// https://wiki.mozilla.org/Telemetry/
+// https://wiki.mozilla.org/Privacy/Reviews/Telemetry
+// https://wiki.mozilla.org/Telemetry
 // https://www.mozilla.org/en-US/legal/privacy/firefox.html#telemetry
 // https://support.mozilla.org/t5/Firefox-crashes/Mozilla-Crash-Reporter/ta-p/1715
 // https://wiki.mozilla.org/Security/Reviews/Firefox6/ReviewNotes/telemetry
-// https://gecko.readthedocs.org/en/latest/toolkit/components/telemetry/telemetry/preferences.html
+// https://gecko.readthedocs.io/en/latest/browser/experiments/experiments/manifest.html
 // https://wiki.mozilla.org/Telemetry/Experiments
 user_pref("toolkit.telemetry.enabled",				false);
 user_pref("toolkit.telemetry.unified",				false);
 user_pref("experiments.supported",				false);
 user_pref("experiments.enabled",				false);
+user_pref("experiments.manifest.uri",				"");
+
+// PREF: Disallow Necko to do A/B testing
+user_pref("network.allow-experiments",			false);
 
 // PREF: Disable the UITour backend
 // https://trac.torproject.org/projects/tor/ticket/19047#comment:3
