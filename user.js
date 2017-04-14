b025942e87a900daccb46b8476de0744863b45fe
nodiscc <nodiscc@gmail.com>
2017-04-14 18:28:39 +0200

    enable Containers feature (privacy.userContext.enabled); add notices about private browsing disabling it fixes #232

--- user.js
+++ user.js
@@ -404,6 +404,11 @@ user_pref("browser.uitour.enabled",				false);
 user_pref("privacy.trackingprotection.enabled",			true);
 user_pref("privacy.trackingprotection.pbmode.enabled",		true);
 
+// PREF: enable contextual identity Containers feature (Firefox >= 52)
+// NOTICE: Containers are not available in Private Browsing mode
+// https://wiki.mozilla.org/Security/Contextual_Identity_Project/Containers
+user_pref("privacy.userContext.enabled",			true);
+
 // PREF: Enable hardening against various fingerprinting vectors (Tor Uplift project)
 // https://wiki.mozilla.org/Security/Tor_Uplift/Tracking
 user_pref("privacy.resistFingerprinting",			true);
@@ -608,6 +613,7 @@ user_pref("network.cookie.thirdparty.sessionOnly",		true);
 // NOTICE: You can not view or inspect cookies when in private browsing: https://bugzilla.mozilla.org/show_bug.cgi?id=823941
 // NOTICE: When Javascript is enabled, Websites can detect use of Private Browsing mode
 // NOTICE: Private browsing breaks Kerberos authentication
+// NOTICE: Disables "Containers" functionality (see below)
 user_pref("browser.privatebrowsing.autostart",			true);
 
 // PREF: Do not store POST data in saved sessions
