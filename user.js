d684068532d3845c5714dc070bbcd1f3efc86d02
pyllyukko <pyllyukko@maimed.org>
2017-06-28 13:00:34 +0300

    Comment out dom.workers.enabled
    This is required at least by https://web.whatsapp.com/

--- user.js
+++ user.js
@@ -16,10 +16,10 @@
 // CVE-2016-5259, CVE-2016-2812, CVE-2016-1949, CVE-2016-5287 (fixed)
 //user_pref("dom.serviceWorkers.enabled",				false);
 
-// PREF: Disable Web Workers
+// PREF: Disable Web Workers (disabled)
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
 // https://www.w3schools.com/html/html5_webworkers.asp
-user_pref("dom.workers.enabled",					false);
+//user_pref("dom.workers.enabled",					false);
 
 // PREF: Disable web notifications (disabled)
 // https://support.mozilla.org/t5/Firefox/I-can-t-find-Firefox-menu-I-m-trying-to-opt-out-of-Web-Push-and/m-p/1317495#M1006501
