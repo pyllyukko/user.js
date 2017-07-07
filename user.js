7f0b82aa1f78f5d61fe5fa522d89be48abc1c9fe
pyllyukko <pyllyukko@maimed.org>
2017-07-07 17:00:14 +0300

    Whitelist "blob" protocol handler

--- user.js
+++ user.js
@@ -339,6 +339,7 @@ user_pref("network.protocol-handler.external.ftp",		false);
 user_pref("network.protocol-handler.external.file",		false);
 user_pref("network.protocol-handler.external.about",		false);
 user_pref("network.protocol-handler.external.chrome",		false);
+user_pref("network.protocol-handler.external.blob",		false);
 user_pref("network.protocol-handler.expose-all",		false);
 user_pref("network.protocol-handler.expose.http",		true);
 user_pref("network.protocol-handler.expose.https",		true);
@@ -348,6 +349,7 @@ user_pref("network.protocol-handler.expose.ftp",		true);
 user_pref("network.protocol-handler.expose.file",		true);
 user_pref("network.protocol-handler.expose.about",		true);
 user_pref("network.protocol-handler.expose.chrome",		true);
+user_pref("network.protocol-handler.expose.blob",		true);
 
 /******************************************************************************
  * SECTION: Extensions / plugins                                                       *
