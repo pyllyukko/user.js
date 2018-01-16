bf7bc620fb7f023982c40cfd765fa891ac3616ca
stablestud <dev@stablestud.org>
2018-01-16 17:23:13 +0100

    Fixed typo in create_locked_pref.sh

--- create_locked_pref.sh
+++ create_locked_pref.sh
@@ -5,18 +5,16 @@ cat > locked_user.js << EOF
 // This file has been created by create_locked_pref.sh
 //
 // It's equivalent to the user.js with the only difference that the settings are
-// locked and cannot be changed by the firefox user or firefox itself.
+// locked and cannot be changed by the Firefox user or Firefox itself.
 //
-// NOTE: Use this in the system-wide configuration only and if you only want the
-//       settings to locked.
-//       See here: https://github.com/pyllyukko/user.js#system-wide-installation-all-platforms
-//
+// NOTE: Use this in the system-wide configuration only and only if you want the
+//       settings to be locked.
+//       Take a look here:
+//  https://github.com/pyllyukko/user.js#system-wide-installation-all-platforms
 /*******************************************************************************
 
 
 EOF
 
-
-sed 's/user_pref("/lockPref("/g' "user.js" >> locked_user.js
-
-echo "Done!"
+sed 's/user_pref("/lockPref("/g' "user.js" >> locked_user.js \
+    && echo "Successfully created locked_user.js."
--- locked_user.js
+++ locked_user.js
@@ -2,12 +2,12 @@
 // This file has been created by create_locked_pref.sh
 //
 // It's equivalent to the user.js with the only difference that the settings are
-// locked and cannot be changed by the firefox user or firefox itself.
+// locked and cannot be changed by the Firefox user or Firefox itself.
 //
-// NOTE: Use this in the system-wide configuration only and if you only want the
-//       settings to locked.
-//       See here: https://github.com/pyllyukko/user.js#system-wide-installation-all-platforms
-//
+// NOTE: Use this in the system-wide configuration only and only if you want the
+//       settings to be locked.
+//       Take a look here:
+//  https://github.com/pyllyukko/user.js#system-wide-installation-all-platforms
 /*******************************************************************************
 
 
