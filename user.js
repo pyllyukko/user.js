ab9c9936188b609551ffef9b632be358105ed2a7
pyllyukko <pyllyukko@maimed.org>
2018-05-05 09:40:10 +0300

    Revert "set accepted languages to en-us, en"
    * This reverts commit 0357cfb6a4eef303cdeb43d532862c81d2a93c15.
    * Fixes #385

--- user.js
+++ user.js
@@ -219,7 +219,7 @@ user_pref("browser.search.geoip.url",				"");
 
 // PREF: Set Accept-Language HTTP header to en-US regardless of Firefox localization
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
-user_pref("intl.accept_languages",				"en-us, en");
+user_pref("intl.accept_languages",				"en-US, en");
 
 // PREF: Don't use OS values to determine locale, force using Firefox locale setting
 // http://kb.mozillazine.org/Intl.locale.matchOS
