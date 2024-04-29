6132732b2ab6743b194748307c6a371f199e0b90
pyllyukko <pyllyukko@maimed.org>
2024-04-29 22:26:50 +0300

    Added yet another link about GPC

--- user.js
+++ user.js
@@ -641,6 +641,7 @@ user_pref("browser.newtabpage.activity-stream.feeds.section.topstories",	false);
 // PREF: Enable Global Privacy Control (GPC) (Firefox >= 120)
 // https://support.mozilla.org/1/firefox/126.0/Linux/en-US/global-privacy-control
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Sec-GPC
+// https://globalprivacycontrol.org/
 user_pref("privacy.globalprivacycontrol.enabled",		true);
 
 /******************************************************************************
