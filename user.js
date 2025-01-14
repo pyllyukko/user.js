pyllyukko/user.js

commit 4b2833eb6a0964d3f007287ffa058f48137f8dcd

Disable the Enterprise Roots preference
Fixes #560

user.js
@@ -1165,6 +1165,12 @@ user_pref("browser.ssl_override_behavior",			1);
 // https://en.wikipedia.org/wiki/Server_Name_Indication#Security_implications_(ESNI)
 user_pref("network.security.esni.enabled",			true);
 
+// PREF: Disable the Enterprise Roots preference
+// https://support.mozilla.org/en-US/kb/how-disable-enterprise-roots-preference
+// https://github.com/pyllyukko/user.js/issues/560
+user_pref("security.certerrors.mitm.auto_enable_enterprise_roots"	false);
+user_pref("security.enterprise_roots.enabled"				false);
+
 /******************************************************************************
  * SECTION: Cipher suites                                                     *
  ******************************************************************************/

commit 44b5d5a60554e4c1896d1dc61ee76b19a32d7610

Fixed user.js syntax error introduced in 4b2833e

user.js
@@ -1168,8 +1168,8 @@ user_pref("network.security.esni.enabled",			true);
 // PREF: Disable the Enterprise Roots preference
 // https://support.mozilla.org/en-US/kb/how-disable-enterprise-roots-preference
 // https://github.com/pyllyukko/user.js/issues/560
-user_pref("security.certerrors.mitm.auto_enable_enterprise_roots"	false);
-user_pref("security.enterprise_roots.enabled"				false);
+user_pref("security.certerrors.mitm.auto_enable_enterprise_roots",	false);
+user_pref("security.enterprise_roots.enabled",				false);
 
 /******************************************************************************
  * SECTION: Cipher suites                                                     *